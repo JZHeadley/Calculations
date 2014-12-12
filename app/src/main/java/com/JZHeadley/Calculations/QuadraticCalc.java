ee39cc4e197310d1263d919bcef506511e4b407b
JZHeadley <zephyrinkg@gmail.com>
2014-12-12 17:12:10 -0500

    Initial commit

---
+++ app/src/main/java/com/JZHeadley/Calculations/QuadraticCalc.java
@@ -0,0 +1,118 @@
+package com.JZHeadley.Calculations;
+
+import java.text.DecimalFormat;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class QuadraticCalc extends ActionBarActivity {
+	public final static String EXTRA_MESSAGE = "com.JZHeadley.MathApp.MESSAGE";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_quadratic_calc);
+
+	}
+
+	public double HandleException (String Variable) {
+		try {
+			double Value = Double.parseDouble(Variable);
+			return Value;
+		}catch(NumberFormatException nfe) {
+			double Answer = Double.POSITIVE_INFINITY;
+			return Answer;
+		}
+	}
+	
+	
+	public void sendNumbers(View view) {
+		// Retrieves variables 
+		DecimalFormat DecimalFormat = new DecimalFormat("#,###.#####");
+		Intent intent = new Intent(this, DisplayRoots.class);
+		EditText editTextA = (EditText) findViewById(R.id.AVariable);
+		EditText editTextB = (EditText) findViewById(R.id.BVariable);
+		EditText editTextC = (EditText) findViewById(R.id.CVariable);
+		String AVariable = editTextA.getText().toString();
+		String BVariable = editTextB.getText().toString();
+		String CVariable = editTextC.getText().toString();
+		// end getting variables
+		
+		if (AVariable == "" || BVariable == "" || CVariable == "") {
+			String Answer = "You did not input one of your values.";
+			intent.putExtra(EXTRA_MESSAGE, Answer);
+		}
+		else {
+			double a = HandleException(AVariable);
+			double b = HandleException(BVariable);
+			double c = HandleException(CVariable);
+			
+			if (a == Double.POSITIVE_INFINITY || b == Double.POSITIVE_INFINITY || c == Double.POSITIVE_INFINITY) {
+	    		String Answer = "You did not enter a number.  Please Try Again.";
+	    		intent.putExtra(EXTRA_MESSAGE, Answer);
+	    	}
+			if (a == 0) {
+				String Answer = "Your A value can not be 0";
+				intent.putExtra(EXTRA_MESSAGE, Answer);
+			}
+			else {
+				double NewB= b *-1;
+				double bInRoot= Math.pow(b,2);
+				double rightRoot= 4 * a * c;
+				double inRoot= bInRoot - rightRoot;
+				double Root= Math.pow(inRoot,(1.0/2));
+				
+				if (inRoot <=-1) {
+					String Answer = "The equation entered does not have any roots.  There is a negative under the square root.";
+					intent.putExtra(EXTRA_MESSAGE, Answer);
+				}
+	
+				else {
+					double plusNumerator= NewB + Root;
+					double minusNumerator= NewB - Root;
+					double denominator= 2 * a;
+					double plusRoot= plusNumerator / denominator;
+					double minusRoot= minusNumerator / denominator;
+					
+					if (plusRoot == Double.NaN || plusRoot == Double.POSITIVE_INFINITY || plusRoot ==  Double.NEGATIVE_INFINITY || minusRoot == Double.NaN || minusRoot == Double.POSITIVE_INFINITY || minusRoot ==  Double.NEGATIVE_INFINITY) {
+						String Answer = "There were problems with the numbers you entered";
+						intent.putExtra(EXTRA_MESSAGE, Answer);
+					}
+					
+					else {
+						String Answer = "The two roots of the equation you entered are " + DecimalFormat.format(plusRoot) + " and " + DecimalFormat.format(minusRoot) + ".";
+						intent.putExtra(EXTRA_MESSAGE, Answer);
+					}
+					
+				}
+				
+			}
+			
+			startActivity(intent);
+		}
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.quadratic_calc, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
